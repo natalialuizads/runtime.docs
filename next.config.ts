9eb8e65fb7cb23f2dc445c0fc4d0af17fff62d74
Natalia <natalialuizads@gmail.com>
2026-02-03 16:46:30 -0300

    fix: corrige path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_PAGES === "true" ? "/runtime.docs" : "",
+  basePath: "/runtime.docs",
   images: {
     unoptimized: true,
   },
